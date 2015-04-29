822156b4ee565841e9562efd51906439f62d3379
Emerson Farrugia <emerson@openmhealth.org>
2015-04-29 19:13:12 +0200

    Add support for other temporal relationship schemas

---
+++ java-schema-sdk/src/main/java/org/openmhealth/schema/domain/TemporalRelationshipToSleep.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2015 Open mHealth
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openmhealth.schema.domain;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import javax.annotation.Nullable;
+import java.util.HashMap;
+import java.util.Map;
+
+
+/**
+ * The temporal relationship of a clinical measure or assessment to sleep.
+ *
+ * @author Emerson Farrugia
+ * @version 1.0
+ * @see <a href="http://www.openmhealth.org/developers/schemas/#temporal-relationship-to-sleep">temporal-relationship
+ * -to-sleep</a>
+ */
+public enum TemporalRelationshipToSleep implements SchemaEnumValue {
+
+    BEFORE_SLEEPING,
+    DURING_SLEEP,
+    ON_WAKING;
+
+    private String schemaValue;
+    private static final Map<String, TemporalRelationshipToSleep> constantsBySchemaValue = new HashMap<>();
+
+    static {
+        for (TemporalRelationshipToSleep constant : values()) {
+            constantsBySchemaValue.put(constant.getSchemaValue(), constant);
+        }
+    }
+
+    TemporalRelationshipToSleep() {
+        this.schemaValue = name().toLowerCase().replace('_', ' ');
+    }
+
+    @Override
+    @JsonValue
+    public String getSchemaValue() {
+        return this.schemaValue;
+    }
+
+    @Nullable
+    @JsonCreator
+    public static TemporalRelationshipToSleep findBySchemaValue(String schemaValue) {
+        return constantsBySchemaValue.get(schemaValue);
+    }
+}
