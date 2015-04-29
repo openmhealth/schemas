b916e961d5b73b23ad47bd4f3f2ba74d15452f82
Emerson Farrugia <emerson@openmhealth.org>
2015-04-29 15:24:54 +0200

    Fix length unit Javadoc

--- java-schema-sdk/src/main/java/org/openmhealth/schema/domain/LengthUnit.java
+++ java-schema-sdk/src/main/java/org/openmhealth/schema/domain/LengthUnit.java
@@ -29,7 +29,7 @@ import java.util.Map;
  *
  * @author Emerson Farrugia
  * @version 1.0
- * @see <a href="http://www.openmhealth.org/developers/schemas/#length-unit">length-unit</a>
+ * @see <a href="http://www.openmhealth.org/developers/schemas/#length-unit-value">length-unit-value</a>
  */
 public enum LengthUnit implements Unit {
 
