daf5cd3ba36a7137d6d0e0c8d401e3b75b74ddba
Emerson Farrugia <emerson@openmhealth.org>
2015-05-20 23:31:08 +0200

    Add support for kcal unit value schema

---
+++ java-schema-sdk/src/main/java/org/openmhealth/schema/domain/omh/KcalUnit.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2015 Open mHealth
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openmhealth.schema.domain.omh;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import javax.annotation.Nullable;
+import java.util.HashMap;
+import java.util.Map;
+
+
+/**
+ * A unit of kilocalories.
+ *
+ * @author Emerson Farrugia
+ * @version 1.0
+ * @see <a href="http://www.openmhealth.org/developers/schemas/#kcal-unit-value">kcal-unit-value</a>
+ */
+public enum KcalUnit implements Unit {
+
+    KILOCALORIE("kcal");
+
+    private String schemaValue;
+    private static final Map<String, KcalUnit> constantsBySchemaValue = new HashMap<>();
+
+    static {
+        for (KcalUnit constant : values()) {
+            constantsBySchemaValue.put(constant.getSchemaValue(), constant);
+        }
+    }
+
+    KcalUnit(String schemaValue) {
+        this.schemaValue = schemaValue;
+    }
+
+    @Override
+    @JsonValue
+    public String getSchemaValue() {
+        return this.schemaValue;
+    }
+
+    @Nullable
+    @JsonCreator
+    public static KcalUnit findBySchemaValue(String schemaValue) {
+        return constantsBySchemaValue.get(schemaValue);
+    }
+}
